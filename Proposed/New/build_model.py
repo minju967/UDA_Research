289b654b7f631b2adac9c43aecdc1100e51683f5
MINJU <minj0329@gmail.com>
2023-04-29 03:39:23 +0900

    Realize "Network" module

--- Proposed/New/build_model.py
+++ Proposed/New/build_model.py
@@ -4,6 +4,7 @@ import functools
 from batchinstancenorm import BatchInstanceNorm2d as Normlayer
 
 
+## DRANet Encoder
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, filters=64, kernel_size=3, stride=1, padding=1):
         super(ResidualBlock, self).__init__()
@@ -26,10 +27,14 @@ class ResidualBlock(nn.Module):
         output = self.main(x)
         output += self.shortcut(x)
         return output
+    
+class DRANet_Encoder(nn.Module):
+    # https://github.com/Seung-Hun-Lee/DRANet
+    # https://arxiv.org/abs/2103.13447
+    # MNIST-M(clf), Cityscapes(seg), GTA5(seg)
 
-class Encoder(nn.Module):
     def __init__(self, channels=3):
-        super(Encoder, self).__init__()
+        super(DRANet_Encoder, self).__init__()
         bin = functools.partial(Normlayer, affine=True)
         self.model = nn.Sequential(
             nn.Conv2d(channels, 32, kernel_size=4, stride=2, padding=1, bias=True),
@@ -40,23 +45,42 @@ class Encoder(nn.Module):
             nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(True),
         )
-        self.pooling = nn.AvgPool2d(kernel_size=112, stride=1)
+        self.Linear = nn.Linear(64*112*112, 64)
+        
 
     def forward(self, x):
-        output = self.model(x)
-        feature = self.pooling(output)
-        return output, feature
+        # f_map: [batch, 64, 112, 112]
+        # global vector: [batch, 64]
+        f_map = self.model(x) 
+        vector = self.Linear(f_map.view(x.shape[0], -1))
+        return f_map, vector
     
-class MLP(nn.Module):
-    def __init__(self, input_dim=64, output_dim=64):
-        super(MLP, self).__init__()
-        self.layer = nn.Sequential(
-            nn.Linear(input_dim, 32),
-            nn.ReLU(),
-            nn.Linear(32, output_dim),
+## Deep InfoMax Encoder
+
+class Vector_convert(nn.Module):
+    # Contribution 1
+    # This module should convert vector to only Content vector or Domain vector
+    def __init__(self, in_size, args):
+        super(Vector_convert, self).__init__()
+        self.args  = args
+        self.network = nn.Sequential(
+            nn.Linear(in_size, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 128),
+            nn.ReLU(True),
+            nn.Linear(128, in_size),
             nn.ReLU()
         )
     
-    def forward(self, x):
-        output = self.layer(x)
-        return output
+    def forward(self, vector1, vector2):
+        # vec_1(dict)
+        # vec_2(dict)
+        content, domain = dict(), dict()
+        for dset in self.args.datasets:
+            content_vec = self.network(vector1[dset])
+            domain_vec = self.network(vector2[dset])
+            
+            content[dset] = content_vec
+            domain[dset]  = domain_vec
+
+        return content, domain
